4fd30fbe22b133a6fc4512d09e293d36aa7cf40a
João Guilherme <flandersbart1329@gmail.com>
2024-04-13 21:46:19 -0300

    update js

---
+++ js/maquinario.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+
+    var btnOrcamento0 = document.getElementsByClassName("orcamento")[0];
+
+
+    btnOrcamento0.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento1 = document.getElementsByClassName("orcamento")[1];
+    
+
+    btnOrcamento1.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento2 = document.getElementsByClassName("orcamento")[2];
+    
+
+    btnOrcamento2.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento3 = document.getElementsByClassName("orcamento")[3];
+    
+
+    btnOrcamento3.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento4 = document.getElementsByClassName("orcamento")[4];
+    
+
+    btnOrcamento4.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento5 = document.getElementsByClassName("orcamento")[5];
+    
+
+    btnOrcamento5.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento6 = document.getElementsByClassName("orcamento")[6];
+    
+
+    btnOrcamento6.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+    var btnOrcamento7 = document.getElementsByClassName("orcamento")[7];
+    
+
+    btnOrcamento7.addEventListener("click", function(event) {
+        alert("Orçamentos enviados por e-mail.");
+    });
+});
